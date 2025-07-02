80d3b8fc7d7be968bdb56ae236bfab4e094ca4f7
prodigitalstrategy <prodigitalstrategy@gmail.com>
2025-07-02 23:24:35 +0300

    Fix markdown blog post loading: fetch from /public for production compatibility

--- src/pages/best-mediterranean-food-near-caesars-palace-las-vegas.jsx
+++ src/pages/best-mediterranean-food-near-caesars-palace-las-vegas.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import MarkdownPostPage from './MarkdownPostPage';
 
 export default function BestMediterraneanFoodNearCaesarsPalace() {
-  return <MarkdownPostPage mdPath={"/content/blog/best-mediterranean-food-near-caesars-palace-las-vegas.md"} />;
+  return <MarkdownPostPage mdPath={"/best-mediterranean-food-near-caesars-palace-las-vegas.md"} />;
 }
--- src/pages/where-to-eat-near-the-sphere-las-vegas.jsx
+++ src/pages/where-to-eat-near-the-sphere-las-vegas.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import MarkdownPostPage from './MarkdownPostPage';
 
 export default function WhereToEatNearTheSphereLasVegas() {
-  return <MarkdownPostPage mdPath={"/content/blog/where-to-eat-near-the-sphere-las-vegas.md"} />;
+  return <MarkdownPostPage mdPath={"/where-to-eat-near-the-sphere-las-vegas.md"} />;
 }
